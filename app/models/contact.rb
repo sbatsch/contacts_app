1b9f09e3c3446b6ab3e6abaea59b9bbfe780a0d9
sbatsch <sbatsch2@gmail.com>
2020-01-20 12:07:37 -0600

    partial created

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ApplicationRecord
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end  
+
+  def friendly_updated_at
+     updated_at.strftime("%b, %d, %Y")
+  end   
 end
